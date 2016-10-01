edb5b6352efc569fbdb766a3d89414b436ba96fe
Stephen Greene <sgreene570@gmail.com>
2016-09-30 22:01:34 -0400

    mhm

--- colorhandler.py
+++ colorhandler.py
@@ -5,13 +5,13 @@ app = Flask(__name__)
 
 @app.route('/set', methods=['GET', 'POST'])
 def colorhandler():
-    with open(str(Path("/dev/pi-blaster").absolute())) as f:
-        red = "22=" + "%.3f" % (int(request.form['redval']) / 255.0)
-        green = "23=" + "%.3f" % (int(request.form['greenval']) / 255.0)
-        blue = "24=" + "%.3f" % (int(request.form['blueval']) / 255.0)
-        f.write(red.encode('utf8'))
-        f.write(green.encode('utf8'))
-        f.write(blue.encode('utf8'))
+    f = open(str(Path("/dev/pi-blaster").absolute()))
+    red = "22=" + "%.3f" % (int(request.form['redval']) / 255.0)
+    green = "23=" + "%.3f" % (int(request.form['greenval']) / 255.0)
+    blue = "24=" + "%.3f" % (int(request.form['blueval']) / 255.0)
+    f.write(red.encode('utf8'))
+    f.write(green.encode('utf8'))
+    f.write(blue.encode('utf8'))
 
     return redirect('/')
 
