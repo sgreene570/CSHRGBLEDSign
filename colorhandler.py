6556cd57a4f2b29d44218c211f17ecc8dfea9c91
Stephen Greene <sgreene570@gmail.com>
2016-09-30 21:39:23 -0400

    fixed utf8 encoding issues

--- colorhandler.py
+++ colorhandler.py
@@ -5,9 +5,13 @@ app = Flask(__name__)
 @app.route('/set', methods=['GET', 'POST'])
 def colorhandler():
     with open("/dev/pi-blaster") as f:
-        print("22=" + str(request.form['redval'] / 255.0), f)
-        print("23=" + str(request.form['greenval'] / 255.0), f)
-        print("24=" + str(request.form['blueval'] / 255.0), f)
+        red = "22=" + "%.3f" % str(request.form['redval'] / 255.0)
+        green = "23=" + "%.3f" % str(request.form['greenval'] / 255.0)
+        blue = "24=" + "%.3f" % str(request.form['blueval'] / 255.0)
+        f.write(red.encode('utf8'))
+        f.write(green.encode('utf8'))
+        f.write(blue.encode('utf8'))
+
     return redirect('/')
 
 
