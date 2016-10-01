511746924a6fc68a8630fe71a28aca0ff1520d60
Stephen Greene <sgreene570@gmail.com>
2016-09-30 21:26:00 -0400

    updated python script to echo out color settings

--- colorhandler.py
+++ colorhandler.py
@@ -4,10 +4,10 @@ app = Flask(__name__)
 
 @app.route('/set', methods=['GET', 'POST'])
 def colorhandler():
-    print(request.form['redval'])
-    print(request.form['greenval'])
-    print(request.form['blueval'])
-
+    with open("/dev/pi-blaster") as f:
+        print("22=" + str(float(request.form['redval'] / 255.0))) >> f
+        print("23=" + str(float(request.form['greenval'] / 255.0))) >> f
+        print("24=" + str(float(request.form['blueval'] / 255.0))) >> f
     return redirect('/')
 
 
