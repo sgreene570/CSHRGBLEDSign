f380ac1df8b483ac18a8902afbd6e18366cbebd6
Stephen Greene <sgreene570@gmail.com>
2016-09-30 20:17:46 -0400

    removed static folder

--- colorhandler.py
+++ colorhandler.py
@@ -6,10 +6,11 @@ app = Flask(__name__)
 def color():
     test = request.form['redval'] + " " + request.form['greeneval'] + " " + request.form['blueval']
     f = open("/dev/pi-blaster")
+    print(test)
     print('echo"22=' + str(request.form['redval'] / 1.0)) >> f
     print('echo"23=' + str(request.form['greenval'] / 1.0)) >> f
     print('echo"24=' + str(request.form['blueval'] / 1.0)) >> f
-    return test
+
 
 if __name__ == "__main__":
     app.run()
