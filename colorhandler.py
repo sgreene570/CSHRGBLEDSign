b7cb72ba32c73bc8744aae74903131e94275106e
Stephen Greene <sgreene570@gmail.com>
2016-10-06 00:49:57 +0000

    added loop feature

--- colorhandler.py
+++ colorhandler.py
@@ -11,6 +11,7 @@ def colorhandler():
     f = open(str(Path("/dev/pi-blaster").absolute()), "w", 0)
     color = request.form['color']
     timer = int(request.form['timer'])
+    loop = int(request.form['loop'])
     red = "22=" + "%.3f" % (int(color[:2], 16) / 255.0)
     green = "23=" + "%.3f" % (int(color[2:4], 16) / 255.0)
     blue = "24=" + "%.3f" % (int(color[4:], 16) / 255.0)
@@ -18,8 +19,8 @@ def colorhandler():
     print(green, file=f)
     print(blue, file=f)
 
-    if timer != 0:
-        for x in range(0, 20):
+    if timer != 0 and loop > 0:
+        for x in range(0, loop):
             time.sleep(timer / 100.0)
             print("22=0", file=f)
             print("23=0", file=f)
