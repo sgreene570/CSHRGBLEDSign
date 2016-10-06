42f6fcbd18178b84db4bb99b5fec5694507a841d
Stephen Greene <sgreene570@gmail.com>
2016-10-06 14:19:22 -0400

    removed web tempalte and converted flask returns to simple strings to allow for API like use

--- colorhandler.py
+++ colorhandler.py
@@ -1,14 +1,18 @@
+"""
+colorhandler.py
+A flask script that handles requests to change GPIO PWN through the pi-blaster library
+Stephen Greene
+"""
 from __future__ import print_function
 from flask import Flask, redirect, request, render_template
 from pathlib import Path
 import time
-import sys
 app = Flask(__name__)
 
 
 @app.route('/set', methods=['GET', 'POST'])
 def colorhandler():
-    f = open(str(Path("/dev/pi-blaster").absolute()), "w", 0)
+    f = open(str(Path("/dev/pi-blaster").absolute()), "w", 0)       # "w" forces file flush every print
     color = request.form['color']
     timer = int(request.form['timer'])
     loop = int(request.form['loop'])
@@ -31,13 +35,13 @@ def colorhandler():
             print(blue, file=f)
             x += 1
 
-    return redirect('/')
+    return "Color: " + request.form[color] + "Timer: " + request.form['timer'] + "Loop: " + request.form['loop']
 
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return "use /set with color, timer, and loop args"
 
 if __name__ == '__main__':
-   app.run(debug=True)
+   app.run()
 
