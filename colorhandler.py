79822bc1564237b13d36537e915be2143a59cbf1
Stephen Greene <sgreene570@gmail.com>
2016-10-13 04:02:14 +0000

    moved functions based on flask usage

--- colorhandler.py
+++ colorhandler.py
@@ -1,8 +1,8 @@
 """
 colorhandler.py
-A flask script that handles requests to change GPIO PWN through the pi-blaster library
-Change pin numbers at the top of setColor method to change which pinouts of the raspi are used
-Stephen Greene
+A flask script that handles requests to change GPIO PWN duty cycle through the pi-blaster library.
+Change pin numbers at the top of the setColor function to change which pinouts of the raspi are used
+Author: sgreene570
 """
 from __future__ import print_function
 from flask import Flask, redirect, request, render_template, jsonify
@@ -21,29 +21,6 @@ def parseColor():
     timer = int(request.form['timer'])
     loop = int(request.form['loop'])
     return setColor(color, timer, loop, f)
-
-
-def setColor(color, timer, loop, f):
-    red = "22=" + "%.3f" % (int(color[:2], 16) / 255.0)
-    green = "23=" + "%.3f" % (int(color[2:4], 16) / 255.0)
-    blue = "24=" + "%.3f" % (int(color[4:], 16) / 255.0)
-    print(red, file=f)
-    print(green, file=f)
-    print(blue, file=f)
-
-    if timer > 0 and loop > 0:
-        for x in range(0, loop):
-            time.sleep(timer / 100.0)        # convert time to milliseconds for blinks
-            print("22=0", file=f)
-            print("23=0", file=f)
-            print("24=0", file=f)
-            time.sleep(timer / 100.0)
-            print(red, file=f)
-            print(green, file=f)
-            print(blue, file=f)
-            x += 1
-
-    return jsonify( {"Color" : str(color)}, {" Timer" : str(timer)}, {"Loop" : str(loop)})
 
 
 @app.route('/set2', methods=['GET', 'POST'])
@@ -96,15 +73,38 @@ def parseString():
     return setColor("%06x" % n, 0, 0, f)
 
 
-def openFile():
-    f = open(str(Path("/dev/pi-blaster").absolute()), "w", 0)        #open file with write premissions and instant flushing
-    return f
-
-
 @app.route('/')
 def index():
     #basic instructions for connection to wrong url rather than 404
     return "/set: color, timer, and loop \n /set2: colorOne, colorTwo, timer, and loop \n /setFade colorOne, colorTwo, timer, and loop"
+
+
+def setColor(color, timer, loop, f):
+    red = "22=" + "%.3f" % (int(color[:2], 16) / 255.0)
+    green = "23=" + "%.3f" % (int(color[2:4], 16) / 255.0)
+    blue = "24=" + "%.3f" % (int(color[4:], 16) / 255.0)
+    print(red, file=f)
+    print(green, file=f)
+    print(blue, file=f)
+
+    if timer > 0 and loop > 0:
+        for x in range(0, loop):
+            time.sleep(timer / 100.0)        # convert time to milliseconds for blinks
+            print("22=0", file=f)
+            print("23=0", file=f)
+            print("24=0", file=f)
+            time.sleep(timer / 100.0)
+            print(red, file=f)
+            print(green, file=f)
+            print(blue, file=f)
+            x += 1
+
+    return jsonify( {"Color" : str(color)}, {" Timer" : str(timer)}, {"Loop" : str(loop)})
+
+
+def openFile():
+    f = open(str(Path("/dev/pi-blaster").absolute()), "w", 0)        #open file with write premissions and instant flushing
+    return f
 
 
 if __name__ == '__main__':
