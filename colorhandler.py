7966baae018d124eaed47d927546227ed7ef3570
Stephen Greene <sgreene570@gmail.com>
2016-10-21 19:38:42 +0000

    fixed formatting to keep lines under 80 chars and look more pythonic

--- colorhandler.py
+++ colorhandler.py
@@ -1,7 +1,9 @@
 """
 colorhandler.py
-A flask script that handles requests to change GPIO PWN duty cycle through the pi-blaster library.
-Change pin numbers at the top of the setColor function to change which pinouts of the raspi are used
+A flask script that handles requests to change GPIO PWN duty cycle 
+with the pi-blaster library.
+Change pin numbers at the top of the setColor function 
+to change which pinouts of the raspi are used
 Author: sgreene570
 """
 from __future__ import print_function
@@ -20,7 +22,10 @@ def parseColor():
     timer = int(request.form['timer'])
     loop = int(request.form['loop'])
     setColor(color, timer, loop)
-    return jsonify({"Function" : "Set color"}, {"Color" : str(color)}, {"Timer" : str(timer)}, {"Loop" : str(loop)})
+    return jsonify({"Function" : "Set color"}, 
+        {"Color" : str(color)}, 
+        {"Timer" : str(timer)}, 
+        {"Loop" : str(loop)})
 
 
 @app.route('/setFade', methods=['POST'])
@@ -30,15 +35,19 @@ def parseFade():
     colorOne = request.form['colorOne']
     colorTwo = request.form['colorTwo']
     setFade(colorOne, colorTwo, timer, loop)
-    return jsonify({"Function" : "Fade"}, {"Starting color" : str(colorOne)},
-        {"Ending color" : str(colorTwo)}, {"Timer" : str(timer)}, {"Loop" : str(loop)})
+    return jsonify({"Function" : "Fade"}, 
+        {"Starting color" : str(colorOne)},
+        {"Ending color" : str(colorTwo)}, 
+        {"Timer" : str(timer)}, 
+        {"Loop" : str(loop)})
 
 
 @app.route('/setString', methods=['POST'])
 def parseString():
     color = stringColor(request.form['string'])
     setColor(color, 0, 0)
-    return jsonify({"Function" : "Set color with string"}, {"Color" : str(color)})
+    return jsonify({"Function" : "Set color with string"}, 
+        {"Color" : str(color)})
 
 
 @app.route('/setStringFade', methods=['POST'])
@@ -48,14 +57,18 @@ def praseStringFade():
     colorOne = stringColor(request.form['stringOne'])
     colorTwo = stringColor(request.form['stringTwo'])
     setFade(colorOne, colorTwo, timer, loop)
-    return jsonify({"Function" : "Fade from Strings"}, {"Starting color" : str(colorOne)},
-        {"Ending color" : str(colorTwo)}, {"Timer" : str(timer)}, {"Loop" : str(loop)})
+    return jsonify({"Function" : "Fade from Strings"}, 
+        {"Starting color" : str(colorOne)},
+        {"Ending color" : str(colorTwo)}, 
+        {"Timer" : str(timer)}, 
+        {"Loop" : str(loop)})
 
 
 @app.route('/')
 def index():
     #basic instructions for connection to wrong url rather than 404
-    return "/set: color, timer, and loop \n /set2: colorOne, colorTwo, timer, and loop \n /setFade colorOne, colorTwo, timer, and loop"
+    return """/set: color, timer, and loop \n /set2: colorOne, colorTwo, timer, 
+        and loop \n /setFade colorOne, colorTwo, timer, and loop"""
 
 
 def setColor(color, timer, loop):
@@ -69,7 +82,7 @@ def setColor(color, timer, loop):
 
     if timer > 0 and loop > 0:
         for x in range(0, loop):
-            time.sleep(timer / 100.0)        # convert time to milliseconds for blinks
+            time.sleep(timer / 100.0)  #convert time to milliseconds for blinks
             print("22=0", file=f)
             print("23=0", file=f)
             print("24=0", file=f)
@@ -81,18 +94,25 @@ def setColor(color, timer, loop):
 
 
 def setFade(colorOne, colorTwo, timer, loop):
-    colorOneVals = [int(colorOne[:2], 16), int(colorOne[2:4], 16), int(colorOne[4:], 16)]
-    colorTwoVals = [int(colorTwo[:2], 16), int(colorTwo[2:4], 16), int(colorTwo[4:], 16)]
-    while (colorOneVals[0] != colorTwoVals[0]) or (colorOneVals[1] != colorTwoVals[1]) or (colorOneVals[2] != colorTwoVals[2]):
+    colorOneVals = [int(colorOne[:2], 16), int(colorOne[2:4], 16), 
+    int(colorOne[4:], 16)]
+
+    colorTwoVals = [int(colorTwo[:2], 16), int(colorTwo[2:4], 16), 
+    int(colorTwo[4:], 16)]
+
+    while ((colorOneVals[0] != colorTwoVals[0])
+        or (colorOneVals[1] != colorTwoVals[1])
+        or (colorOneVals[2] != colorTwoVals[2])):
         hexColor = ""
         for x in range(0, 3):
             if colorOneVals[x] > colorTwoVals[x]:
                 colorOneVals[x] -= 1
             elif colorOneVals[x] < colorTwoVals[x]:
                 colorOneVals[x] += 1
-            if colorOneVals[x] < 10:                             #format hex digits in string correctly
+            if colorOneVals[x] < 10:      #format hex digits in string correctly
                 hexColor += "0"
-            hexColor += str(hex(colorOneVals[x]))[2:]            #remove front 2 chars of hex value
+            #remove front 2 chars of hex value
+            hexColor += str(hex(colorOneVals[x]))[2:]
         setColor(hexColor, timer, loop)
 
 
@@ -104,7 +124,8 @@ def stringColor(string):
 
 
 def openFile():
-    f = open(str(Path("/dev/pi-blaster").absolute()), "w", 0)        #open file with write premissions and instant flushing
+    #open file with write premissions and instant flushing
+    f = open(str(Path("/dev/pi-blaster").absolute()), "w", 0)
     return f
 
 
