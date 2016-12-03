c3b793416a489a462586617dd9dc4c4e5d2ef3e7
Stephen Greene <sgreene570@gmail.com>
2016-12-03 21:03:08 +0000

    added rate limiting

--- colorhandler.py
+++ colorhandler.py
@@ -4,33 +4,40 @@ A flask script that handles http requests to change GPIO PWN duty cycle
 with the pi-blaster library.
 Author: Stephen Greene
 """
-from __future__ import print_function
 from flask import Flask, redirect, request, render_template, jsonify
-from pathlib import Path
-import time, hashlib
 from flask_restful import Resource, Api
+from pathlib import Path
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+import time, hashlib, os
 
 
 app = Flask(__name__)
+limiter = Limiter(
+    app,
+    key_func=get_remote_address)
 
 
 REDPIN = 22
 GREENPIN = 23
 BLUEPIN = 24
-f = open(str(Path("/dev/pi-blaster").absolute()), "w", 0)
+currColor = "000000"
 
 
 @app.route("/set", methods=["POST", "GET"])
+@limiter.limit("4 per second")
 def parseColor():
     color = request.form["color"]
     try:
         setColor(color)
     except ValueError:
-        color = stringColor(color)
+        color = "C1007C"
         setColor(color)
+        return jsonify({"Input error" : "Default color used"},
+                {"Color" : color})
 
     return jsonify({"Function" : "Set color"},
-        {"Color" : str(color)})
+        {"Color" : color})
 
 
 @app.route("/setOff")
@@ -44,49 +51,20 @@ def index():
     #basic instructions for connection to /
     return jsonify({"/" : "MOLS Help Request"},
         {"/set" : "Color=FFFFFF (6 digit hex format, no # sign)"},
-        {"/setOff" : "No params: turns lights off (hex value 000000)"})
+        {"/setOff" : "No params: turns lights off (hex value 000000)"},
+        {"Current color" : currColor})
 
 
 def setColor(color):
+    global currColor
+    currColor = color
     red = str(REDPIN) + "=" + "%.3f" % (int(color[:2], 16) / 255.0)
     green = str(GREENPIN) + "=" + "%.3f" % (int(color[2:4], 16) / 255.0)
     blue = str(BLUEPIN) + "=" + "%.3f" % (int(color[4:], 16) / 255.0)
-    print(red, file=f)
-    print(green, file=f)
-    print(blue, file=f)
-
-
-def setFade(colorOne, colorTwo):
-    colorOneVals = [int(colorOne[:2], 16), int(colorOne[2:4], 16),
-    int(colorOne[4:], 16)]
-
-    colorTwoVals = [int(colorTwo[:2], 16), int(colorTwo[2:4], 16),
-    int(colorTwo[4:], 16)]
-
-    while ((colorOneVals[0] != colorTwoVals[0])
-        or (colorOneVals[1] != colorTwoVals[1])
-        or (colorOneVals[2] != colorTwoVals[2])):
-        hexColor = ""
-        for x in range(0, 3):
-            if colorOneVals[x] > colorTwoVals[x]:
-                colorOneVals[x] -= 1
-            elif colorOneVals[x] < colorTwoVals[x]:
-                colorOneVals[x] += 1
-            if colorOneVals[x] < 10:      #format hex digits in string correctly
-                hexColor += "0"
-            #remove front 2 chars of hex value
-            hexColor += str(hex(colorOneVals[x]))[2:]
-        setColor(hexColor)
-
-
-#Hash a given string to a 6 digit hex color
-def stringColor(string):
-    h = hashlib.md5(string)
-    n = int(h.hexdigest(), 16)
-    n = n % (256 ** 3)
-    return "%06x" % n
+    os.system("echo " + red + " >> /dev/pi-blaster")
+    os.system("echo " + green + " >> /dev/pi-blaster")
+    os.system("echo " + blue + " >> /dev/pi-blaster")
 
 
 if __name__ == "__main__":
    app.run()
-
